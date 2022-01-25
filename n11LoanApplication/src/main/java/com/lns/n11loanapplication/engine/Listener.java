a2c536e199ae9979aaae33856a41678786e13e9e
kevserdogan <veyseldoganbm@gmail.com>
2022-01-26 02:07:24 +0300

    sended sms use twilo with kafka after the users credit approve

--- n11LoanApplication/src/main/java/com/lns/n11loanapplication/engine/Listener.java
+++ n11LoanApplication/src/main/java/com/lns/n11loanapplication/engine/Listener.java
@@ -4,6 +4,7 @@ import com.lns.n11loanapplication.data.constants.CreditsConstans;
 import com.lns.n11loanapplication.data.dto.UserCreditDto;
 import com.lns.n11loanapplication.service.CreditService;
 import com.lns.n11loanapplication.service.informationService.SendMailService;
+import com.lns.n11loanapplication.service.informationService.SendSmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -15,7 +16,8 @@ public class Listener {
     CreditService creditService;
     @Autowired
     SendMailService sendMailService;
-
+    @Autowired
+    SendSmsService sendSmsService;
 
     public Listener(CreditService creditService) {
         this.creditService = creditService;
@@ -26,5 +28,6 @@ public class Listener {
     public void calculateCreditScoreListener(@Payload String userTckn) {
         UserCreditDto userCreditDto=   creditService.calculateCreditLimit(userTckn);
         sendMailService.sendInformation("doganvey@outlook.com", CreditsConstans.getCreditLimitResultMessage() +userCreditDto.getCreditAmount().toString());
+        sendSmsService.sendInformation("+905357479474",CreditsConstans.getCreditLimitResultMessage() +userCreditDto.getCreditAmount().toString());
     }
 }
